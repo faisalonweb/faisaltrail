0abba2687d886a33de5f861db4a2dff4dc84d6c2
Faisal Usman <faisal.usman@prixite.com>
2022-07-13 11:49:05 +0500

    Added hoc for private routes and notfoundpage as well

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,22 @@ import SignUpPage from 'src/components/common/Presentational/SignUpPage/SignUpPa
 import LoginPage from 'src/components/common/Presentational/LoginPage/LoginPage';
 import ExplorePage from './components/common/Smart/Explore/Explore';
 import TrailInfo from 'src/components/common/Smart/TrailInfo/TrailInfo';
-import Help from 'src/components/common/Smart/Help/Help';
+import NotFoundPage from 'src/components/shared/NotFoundPage/NotFoundPage';
+import TokenRestrictedRouter from 'src/components/hoc/TokenRestrictedRouter';
 import { Routes, Route } from 'react-router-dom';
+import PrivateRoute from 'src/routes/PrivateRoute';
 import CssBaseline from '@mui/material/CssBaseline';
-import './App.css';
 import Trails from './components/common/Smart/Trails/Trails';
 import TrailDetail from './components/common/Presentational/TrailDetail/TrailDetail';
+import './App.css';
+
 
 function App() {
   useEffect(() => {
+    const storeMe = {
+      myBool: true
+    }
+    localStorage.setItem('test', JSON.stringify(storeMe))
     const initialState = localStorage.getItem('theme');
     if (!initialState) {
       localStorage.setItem('theme', 'light');
@@ -30,9 +37,10 @@ function App() {
           <Route path='/login' element={<LoginPage />} />
           <Route path='/explore' element={<ExplorePage />} />
           <Route path='/trails' element={<Trails />} />
-          <Route path='/help' element={<Help />} />
           <Route path='/trails/trail-info/:id' element={<TrailInfo />} />
           <Route path='/trails/trail-details/:id' element={<TrailDetail />} />
+          <Route path="*"  element={<TokenRestrictedRouter component={PrivateRoute}/>}/>
+          <Route path="/notpagefound" element={<NotFoundPage />} />
         </Routes>
       </Layout>
     </div>
